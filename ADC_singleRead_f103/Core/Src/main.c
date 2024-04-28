b9c5854c35b06f93995049cbc617b56adf923351
Dmitriy <slagterra2017@yandex.ru>
2024-04-28 15:58:01 +0300

    adc lab is ended

--- ADC_singleRead_f103/Core/Src/main.c
+++ ADC_singleRead_f103/Core/Src/main.c
@@ -20,7 +20,7 @@
 #include "stm32f103xb.h"
 
 void delay(){
-	for(uint32_t i=0; i < 1000000; i++){}
+	for(uint32_t i=0; i < 500000; i++){}
 }
 
 void setupRCCTo72MHz(){
@@ -41,13 +41,11 @@ void setupRCCTo72MHz(){
 	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV8;
 
 }
-
-uint16_t ADC_Read(){
-	uint16_t temp =  0;
+uint32_t ADC_Read(){
 	ADC1->CR2 |= ADC_CR2_SWSTART; // start adc convert
-	while(!(ADC1->SR & ADC_SR_EOC)); //wait for end of convert
-	temp = ADC1->DR; // read adc data
-	return temp;
+    while(!(ADC1->SR & ADC_SR_EOC)){}; //wait for end of convert
+
+	return ADC1->DR; // read adc data
 }
 
 
@@ -56,7 +54,9 @@ int main(void){
 	RCC->APB2ENR |= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_ADC1EN);  // turn on clock on port A and ADC1
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN; // turn on clock on alternative funcs (interrupts)
 	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN; // turn on clock of timer 1
-	//setup PA8-PA11 to alternative push-pull output with max speed 50 MHz (need for PWM)
+	//setup PA0 to analog mode
+	GPIOA->CRL &= ~(GPIO_CRL_MODE0 | GPIO_CRL_CNF0); //reset PA0 to analog
+	//setup PA8 to alternative push-pull output with max speed 50 MHz (need for PWM)
 		GPIOA->CRH &= ~(GPIO_CRH_MODE8 | GPIO_CRH_CNF8); //reset PA8
 
 		GPIOA->CRH |= (GPIO_CRH_MODE8_0 | GPIO_CRH_MODE8_1 | GPIO_CRH_CNF8_1 ); //alternative push-pull output with max speed 50 MHz
@@ -66,34 +66,14 @@ int main(void){
 		TIM1->CR1 &= ~(TIM_CR1_OPM); //one pulse mode off (Counter will not stop on every update)
 		TIM1->CR1 &= ~(TIM_CR1_DIR); //counter used as upcounter
 		TIM1->CR1 &= ~(TIM_CR1_CMS); // edge-aligned mode
-
-		//TIM1->CR1 |= TIM_CR1_ARPE; //TIM1_ARR register is buffered
-
-		// setup update interrupt
-	//	TIM1->DIER |= TIM_DIER_UIE; // update interrupt enable
 		// setup timer freq
-		TIM1->PSC = 4 - 1; // 72Mhz / 4 = 18MHz -> first prescaler
-		TIM1->ARR = 4096 - 1; // 18MHz / 500 = 36kHz -> second prescaler
-		// (max tick count in one PWM Pulse) --> finish freq = 36kHz,
-
-	//	NVIC_EnableIRQ(TIM1_UP_IRQn); //enable update irq
-
-
-
+		TIM1->PSC = 1 - 1; // 72Mhz / 1 = 72MHz -> first prescaler
+		TIM1->ARR = 4096 - 1; // 72MHz / 4096 = 17.57kHz -> second prescaler
+		// (max tick count in one PWM Pulse) --> finish freq = 17.57kHz,
 		//setup PWM
 		////setup chanels
 		TIM1->CCMR1 &= ~(TIM_CCMR1_CC1S); // chanel 1 to output
 		TIM1->BDTR |= TIM_BDTR_MOE; // assing timer output to pins
-	//	////setup preload
-	//	TIM1->CCMR1 |= (TIM_CCMR1_OC1PE); // chanel 1 preload enable
-	//	TIM1->CCMR1 |= (TIM_CCMR1_OC2PE); // chanel 2 preload enable
-	//	TIM1->CCMR2 |= (TIM_CCMR2_OC3PE); // chanel 3 preload enable
-	//	TIM1->CCMR2 |= (TIM_CCMR2_OC4PE); // chanel 4 preload enable
-	//	//OCxRef is not affected by the ETRF Input
-	//	TIM1->CCMR1 &= ~TIM_CCMR1_OC1CE;
-	//	TIM1->CCMR1 &= ~TIM_CCMR1_OC2CE;
-	//	TIM1->CCMR2 &= ~TIM_CCMR2_OC3CE;
-	//	TIM1->CCMR2 &= ~TIM_CCMR2_OC4CE;
 		//// setup PWM mode 1
 		TIM1->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1); // chanel 1 to PWM mode 1
 		//turn on PWM (active -> high)
@@ -104,16 +84,31 @@ int main(void){
 
 		TIM1->CR1 |= TIM_CR1_CEN; // start timer
 	// setup adc
-	ADC1->CR2 |= (ADC_CR2_EXTSEL_2 | ADC_CR2_EXTSEL_1 | ADC_CR2_EXTSEL_0); // adc started on SWSTART bit
-	ADC1->CR2 |= (ADC_CR2_EXTTRIG); // enable external adc turn on for single convert
 	ADC1->CR2 |= (ADC_CR2_ADON); // turn on adc
-	// adc calib
-	ADC1->CR2 |= (ADC_CR2_CAL); // start calib
-	while(!(ADC1->SR & ADC_CR2_CAL)); //wait for end of calib
+	//	 adc calib
+//	ADC1->CR2 |= (ADC_CR2_CAL); // start calib
+//	while(!(ADC1->SR & ADC_CR2_CAL)); //wait for end of calib
+	//разрешаем прерывание от АЦП
+//	NVIC_EnableIRQ(ADC1_2_IRQn);
+//	//для первого канала между выборками 7.5 цикла
+//	ADC1->SMPR2 = ADC_SMPR2_SMP1_0;
+//	//разрешаем прерывания по окончанию преобразования
+//	ADC1->CR1 |= ADC_CR1_EOCIE;
+	ADC1->CR2 |= (ADC_CR2_EXTSEL); // adc started on SWSTART bit
+	ADC1->CR2 |= (ADC_CR2_EXTTRIG); // enable external adc turn on for single convert
+	ADC1->CR2 &= ~(ADC_CR2_CONT); // continious mode
+	// выбор каналов
+	ADC1->SQR1 =0; // 1 регулярный канал
+	ADC1->SQR2 = 0x00000000;
+	ADC1->SQR3 =0x00000000; // 1 преобразование - канал 0
+	ADC1->CR2 |= ADC_CR2_SWSTART; // start adc convert
+
 
 	while(1){
-		uint16_t res = ADC_Read(); // read adc data
+		uint32_t res = ADC_Read();
 		TIM1->CCR1 = res;
+		//TIM1->CCR1 = ADC_Read();
+//		delay();
 
 	}
 }
